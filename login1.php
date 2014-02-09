56afa713aff0d0c431b996ba049ff1f60f7dbb89
areen <areen_darawsheh@hotmail.com>
2014-02-10 00:57:00 +0300

    sjsjsjsjs

--- login1.php
+++ login1.php
@@ -3,47 +3,14 @@
 
 
         <link rel='stylesheet' type='text/css' href='styles.css' />
+        	<link rel="stylesheet" type="text/css" href="http://areen.dc7.us/New%20folder/images.css" />
         <script src='http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'></script>
         <script type='text/javascript' src='menu_jquery.js'></script>
         <meta charset=utf-8 />
         <script type="text/javascript" src="http://areen.dc7.us/New%20folder/ajax.js"></script>
 
         <style  type="text/css">
-            .art-image
-            {
-                width: 10px;
-                height: 10px;
-                right:0px;
-                top:0px;
-                z-index:-1;
-            }
-            .art-img
-            {
-                width: 10px;
-                height: 10px;
-
-
-                z-index:0;
-                margin-left:140px;
-
-                top:166px;
-                right:580px;
-            }
-            .zzzz
-            {
-
-                text-align:left;
-                font-size:13pt;
-                font-family: Verdana, Geneva, Arial, Helvetica, sans-serif;
-
-            }
-            .styled-select {
-                width: 240px;
-                height: 34px;
-                overflow: hidden;
-                background: url(new_arrow.png) no-repeat right #ddd;
-                border: 1px solid #ccc;
-            }
+         
         </style>
 
         <script language="JavaScript">
@@ -75,13 +42,13 @@
             while ($row = mysql_fetch_array($result)) {
                 ///// retreiving image from mysql :
 
-                echo" <img class='art-image' src='http://areen.dc7.us/New%20folder/Capture.jpg'>";
-                echo"  <img  class='art-img' src='http://areen.dc7.us/New%20folder/imag.php?SN1=" . $row["student_id"] . "'  />";
+                echo" <img class='art-image'id='img1' src='http://areen.dc7.us/New%20folder/Capture.PNG'>";
+                echo"  <img  class='art-img' id='img2' src='http://areen.dc7.us/New%20folder/imag.php?SN1=" . $row["student_id"] . "'  />";
 
                 echo" </br>";
-                echo "<div   class='zzzz'>Student Name: " . $row["student_name"] . "</br></div>";
-                echo "<div   class='zzzz'>Academic Dept: " . $row["dept"] . "</br></div>";
-                echo "<div   class='zzzz'>ID : " . $row["student_id"] . "</br></div>";
+                echo "<div   class='zzzz' >Student Name: " . $row["student_name"] . "</br></div>";
+                echo "<div   class='zzzz' >Academic Dept: " . $row["dept"] . "</br></div>";
+                echo "<div   class='zzzz' >ID : " . $row["student_id"] . "</br></div>";
 
                 echo "<input style='display:none' id='my_id'  name='areen' size='13' hight='10' value=" . $row["student_id"] . " />";
                 ECHO "</br>";
@@ -90,7 +57,7 @@
             <select id="myList" class="styled-select" name="soso" >
                  <option>Select..</option>
                 <option>First_ 2013</option>
-                <option>second_2013</option>  
+                <option>second _2013</option>  
                 <option>summer_2013</option>  
             </select>
             
